83e084a3a298b3980a300074a9f29ec5f3208d0a
Soy-developers <dinahsoita355@gmail.com>
2025-07-25 09:51:59 +0300

    first commit

---
+++ app/src/main/java/com/example/taytay/models/UserModel.kt
@@ -0,0 +1,7 @@
+package com.example.taytay.models
+
+data class UserModel(
+    val username: String="",
+    val email: String="",
+    val userId: String=""
+)
---
+++ app/src/main/java/com/example/taytay/ui/theme/screens/dashboard/dashboardScreen.kt
@@ -0,0 +1,48 @@
+package com.example.taytay.ui.theme.screens.dashboard
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DashboardScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "DASHBOARD",
+            fontSize = 40.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontStyle = FontStyle.Normal,
+            color = Color.Green,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+//            .background(Color.LightGray)
+                .padding(5.dp)
+        )
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DashboardScreenPreview(){
+    DashboardScreen(rememberNavController())
+}
